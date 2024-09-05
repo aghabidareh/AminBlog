6c53e77492b4727cb61d52b6d5bc66860cdc9ffa
aghabidareh <aghabidareh@gmail.com>
2024-09-05 12:06:05 +0330

    set login/register and forgot password route and front

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AuthController::class,'login'])->name('login');
+Route::get('register', [AuthController::class,'register'])->name('register');
+Route::get('forgot-password', [AuthController::class,'forgetPassword'])->name('forgot-password');
